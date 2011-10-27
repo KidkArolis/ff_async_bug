efcfb069131bc7fa679d614c388b865f080d2c94
Karolis Narkevicius <karolis.n@gmail.com>
2011-10-27 16:20:51 +0100

    Don't use localhost

--- ff_async_bug2.js
+++ ff_async_bug2.js
@@ -1,3 +1,3 @@
 var s = document.createElement("script");
-s.src = "http://localhost/ff_async_bug/empty.js" + "?" + new Date().getTime();
+s.src = "empty.js" + "?" + new Date().getTime();
 document.head.appendChild(s);
